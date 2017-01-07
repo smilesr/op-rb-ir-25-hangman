146149315ef88bb9cf1524b1af7aa99b8566fe64
Robert Smiles Jr <smilesr120@gmail.com>
2017-01-07 18:30:53 -0500

    added validation for input length; added comments

--- hangman.rb
+++ hangman.rb
@@ -1,13 +1,15 @@
 require 'json'
 require 'fileutils'
 require 'pry'
+require 'pry-byebug'
 $dir = 'saved_games'
 
 class SecretWord
-  attr_accessor :secretword
+  attr_reader :secretword
   def initialize
     @secretword = select_word
     puts @secretword
+    # binding.pry
   end
 
   def select_word
@@ -18,21 +20,21 @@ class SecretWord
     return chosen_line.downcase.strip!
   end   
 end
-
-
+#*****************************************************
 class Play
+  #1
   def initialize(secret, display_board, x)
-    @secret = secret
+    @secret = secret #instance of Secret
     @correct_letters_arr = []
     @incorrect_letters_arr = []
-    @display = display_board
+    @display = display_board #instance of Display
     @x = x
-    @play = self
+    @play = self #instance of Play
   end
-
+  #2
   def ask_for_letter
     guess = nil
-    until (guess =~ /[a-z]/)
+    until (guess =~ /[a-z]/ && guess.length == 1)
       puts "Guess a letter or type 'save' to save game: "
       guess = gets.downcase.chomp
       if guess == 'save'
@@ -41,11 +43,14 @@ class Play
       if guess !~ /[a-z]/
         puts "That is not a letter."
       end
+      if guess.length > 1
+        puts "Only type one letter."
+      end
     end
     display_word = find_letter(guess)
     display_word
   end
-
+  #3
   def find_letter(guess)
     if @correct_letters_arr.include?(guess) || @incorrect_letters_arr.include?(guess)
       puts "You have already guessed that letter"
@@ -56,27 +61,27 @@ class Play
     else
       incorrect_letters(guess)
     end
-
   end
-
+  #4
   def incorrect_letters(guess)
     @incorrect_letters_arr << guess
     @display.turns(@incorrect_letters_arr)
   end
-
+  #5
   def correct_letters(guess)
     @correct_letters_arr << guess
     display_word = @display.show_word(guess)
   end
 end
-
+# *******************************************************
 class Display
+  #1
   def initialize (secret)
-    @secret = secret.secretword
+    @secret = secret.secretword #instance of Secret
     @display_word = blank_word
     @turns = 6
   end
-
+  #2
   def blank_word
     display_word =''
     for pos in 0..@secret.length - 1
@@ -84,7 +89,7 @@ class Display
     end
     display_word
   end
-
+  #3
   def show_word(guess)
     for pos in 0..@secret.length - 1
       if @secret[pos] == guess
@@ -94,7 +99,7 @@ class Display
     puts @display_word
     @display_word
   end
-
+  #4
   def turns(arr)
     @turns -= 1
     puts "Wrong."
@@ -105,7 +110,7 @@ class Display
     puts "You have #{@turns} wrong guesses left."
     @turns
   end
-
+  #5
   def winner_loser(display_word)
     # display_word parameter used here rather than @display_word to address bug in which recovered saved-game fails to update @display_word
     if display_word == @secret
@@ -117,7 +122,7 @@ class Display
     end
   end
 end
-
+#*********************************************************
 class SaveGame
   def save_game secret, display, play
     puts "name your saved game:"
@@ -139,7 +144,6 @@ class SaveGame
   end
 end
 
-
 puts "Do you want to start a new game or continue a saved game?"
 puts "Type 'new' or 'saved':"
 answer = gets.downcase.chomp
@@ -151,7 +155,7 @@ if answer == 'new'
   play_now = Play.new(secret, display_board, x)
 else
   Dir.chdir($dir) do
-    puts "which game do you want to recover?"
+    puts "which game do you want to recover from this list?"
     (Dir["*_secret.txt"]).each{|item| puts item.scan(/^[a-z1-9]+/)}
     filename = gets.downcase.chomp
     file = File.open("#{filename}_secret.txt", "r")
