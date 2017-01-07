fb9c2d84a5d0f0f850e840d5124f275639e28ba1
Robert Smiles Jr <smilesr120@gmail.com>
2017-01-06 21:40:50 -0500

    added capability to save and restore state

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,7 @@
-require 'pry'
+require 'json'
 
 class SecretWord
   attr_accessor :secretword
-
   def initialize
     @secretword = select_word
     puts @secretword
@@ -19,23 +18,29 @@ end
 
 
 class Play
-  def initialize(secret, display_board)
-    @secret = secret.secretword
+  def initialize(secret, display_board, x)
+    @secret = secret
     @correct_letters_arr = []
     @incorrect_letters_arr = []
     @display = display_board
+    @x = x
+    @play = self
   end
 
   def ask_for_letter
     guess = nil
     until (guess =~ /[a-z]/)
-      puts "Guess a letter: "
+      puts "Guess a letter or type 'save' to save game: "
       guess = gets.downcase.chomp
+      if guess == 'save'
+        @x.save_game @secret, @display, @play
+      end
       if guess !~ /[a-z]/
         puts "That is not a letter."
       end
     end
-    find_letter(guess)
+    display_word = find_letter(guess)
+    display_word
   end
 
   def find_letter(guess)
@@ -43,7 +48,7 @@ class Play
       puts "You have already guessed that letter"
       ask_for_letter
     end
-    if @secret.include?(guess)
+    if @secret.secretword.include?(guess)
       correct_letters(guess)
     else
       incorrect_letters(guess)
@@ -57,7 +62,7 @@ class Play
 
   def correct_letters(guess)
     @correct_letters_arr << guess
-    @display.show_word(guess)
+    display_word = @display.show_word(guess)
   end
 end
 
@@ -83,6 +88,7 @@ class Display
       end
     end
     puts @display_word
+    @display_word
   end
 
   def turns
@@ -91,8 +97,11 @@ class Display
     puts "You have #{@turns} wrong guesses left"
   end
 
-  def winner_loser
-    if @display_word == @secret
+  def winner_loser(display_word)
+    # display_word parameter used here rather than @display_word to address bug in which recovered saved-game fails to update @display_word
+    puts @display_word
+    puts display_word
+    if display_word == @secret
       return 1
     elsif  @turns == 0
       return 2
@@ -102,13 +111,45 @@ class Display
   end
 end
 
-secret = SecretWord.new
-display_board = Display.new(secret)
-play_now = Play.new(secret, display_board)
+
+class SaveGame
+  def save_game secret, display, play
+    file_arr=[["savedgame_secret.txt", secret],["savedgame_display.txt",display],["savedgame_play.txt",play]]
+    file_arr.each do |item|
+      File.open(item[0],"w+") do |f|
+        f.write(Marshal.dump(item[1]))
+        f.close
+      end
+    end
+    exit
+  end
+end
+
+puts "Type 'new' or 'saved':"
+answer = gets.downcase.chomp
+
+if answer == 'new'
+  secret = SecretWord.new
+  display_board = Display.new(secret)
+  x = SaveGame.new
+  play_now = Play.new(secret, display_board, x)
+else
+  file = File.open("savedgame_secret.txt", "r")
+  secret = Marshal.load(file.read)
+  file.close
+  file = File.open("savedgame_display.txt", "r")
+  display_board = Marshal.load(file.read)
+  file.close
+  file = File.open("savedgame_play.txt", "r")
+  play_now = Marshal.load(file.read)
+  file.close
+end
+
 finished = 0
 until finished > 0
-  play_now.ask_for_letter
-  finished = display_board.winner_loser
+  display_word = play_now.ask_for_letter
+  
+  finished = display_board.winner_loser(display_word)
   if finished == 1
     puts "YOU WIN!"
   elsif finished == 2
